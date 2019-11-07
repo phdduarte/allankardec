fda8f483baf366dc35ebbc5a62630a86217b8725
phdduarte <phdduarte@hotmail.com>
2019-11-06 22:51:37 -0300

    Add imagem e view do mapa do site

---
+++ resources/views/manuscritos/mapadosite.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                MAPA DO SITE
+                </div>
+                <div class="panel-body">
+                    @if(Session::has('mensagem_sucesso')) 
+                        <div class="alert alert-danger">{{Session::get('mensagem_sucesso')}}</div>
+                    @endif  
+                    <div>
+                        <img src=".images/mapadosite.png" alt="MAPA DO SITE">	
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
