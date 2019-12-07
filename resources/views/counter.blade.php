e8b50766701bc953f97fcee8fd570bbcb749bc56
phdduarte <phdduarte@hotmail.com>
2019-12-07 20:48:45 -0300

    fix contador

--- resources/views/counter.blade.php
+++ resources/views/counter.blade.php
@@ -10,13 +10,16 @@
               <span id="countup" class="big-number">0</span>
             </div>
             <div class="center pb2">
-              <span>Total do acervo</span>
+              <span>Total de páginas do acervo</span>
             </div>
             <div class="center justify">
               <p>O nosso museu digital se encontra constantemente sendo atualizado com novos documentos de interesse para o Espiritismo. Verifique sempre as nossas atualizações com novas fontes primárias que propiciam material de subsídio para pesquisas e estudos sobre a Doutrina Espírita. 
               </p><br>
-              <p>
-              Compartilhe e divulgue nosso museu digital: <a href="https://www.allankardec.online/">AllanKardec.online</a>
+            </div>
+            <div>
+              <p class="center">
+                Compartilhe e divulgue nosso museu digital: 
+                <a href="https://www.allankardec.online/" class="white h3">AllanKardec.online</a>
               </p>
             </div>
           </div>
